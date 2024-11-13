3ff48161cb655b675ce55599fab2a28a77ed0ad0
apixys <argentumapixys@gmail.com>
2024-11-13 15:27:35 -0800

    delete user router

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ require('dotenv').config({
 })
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const accountsRouter = require('./routes/accounts');
 
 const app = express();
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/accounts', accountsRouter);
 
 module.exports = app;
