2bb423e8b18fcce91f51b54aa394efd75ebc7de8
apixys <argentumapixys@gmail.com>
2024-11-13 12:48:38 -0800

    dotnet setup

--- server/app.js
+++ server/app.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+    path: '../.env',
+})
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,20 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    database: 'bronco',
-    user: 'root',
-    password: process.env.DB_ROOT_PASSWORD,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     connectionLimit: 10,
 })
 
 async function query(sql) {
-    try {
-        const [results,] = await pool.query(sql);
+    console.log(process.env.DB_USER);
+    pool.query(sql, (err, results) => {
+        if (err) console.log(err);
         return results;
-    } catch (err) {
-        console.error(err.message)
-    }
+    })
 }
 
 module.exports = { query };
