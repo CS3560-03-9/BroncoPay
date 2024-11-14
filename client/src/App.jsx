5ba86a15871ebaee5348f9954fdc5d135f1c2252
Jacob Lembach <59783225+Sweetzzzz@users.noreply.github.com>
2024-11-14 11:05:07 -0800

    added nav bar to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ButtonUsage from './components/button'
+import BasicGrid from './components/navBar'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import ResponsiveAppBar from './components/navBar'
+import BasicTable from './components/dataDisplay'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +37,13 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      <ResponsiveAppBar></ResponsiveAppBar>
+      {/* <BasicTable></BasicTable> */}
+
+      {/* <ButtonUsage/> */}
+      
     </>
   )
 }
