ac29944d63a5427270a8f29d3dbb64cfc2529b0f
apixys <argentumapixys@gmail.com>
2024-11-15 00:04:34 -0800

    express-validator added

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -4,10 +4,10 @@ async function getTransactionsByAccount(handler) {
 	return await db.query('SELECT * FROM `transactions` WHERE `from_handler` = ? OR `to_handler` = ?', [handler, handler]);
 }
 
-async function createTransaction(transactionData) {
-	const {from, to, amount, desc} = transactionData;
+async function createTransaction(body) {
+	// TODO change balance of accounts' balances
 	return await db.query('INSERT INTO `transactions` (`from_handler`, `to_handler`, `amount`, `transaction_desc`) VALUES (?, ?, ?, ?)',
-		[from, to, amount, desc]);
+		[body.fromHandler, body.toHandler, body.amount, body.description]);
 }
 
 module.exports = {
