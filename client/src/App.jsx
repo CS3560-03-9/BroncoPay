6846a0ab7c573d98b0642dc21eefaa082cbde26b
Sweetzzzz <jacob.lembach@gmail.com>
2024-11-15 21:04:41 -0800

    added charts to dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,6 +40,7 @@ function App() {
       {/* <BasicTable /> */}
       {/* <ButtonUsage/> */}
       <Outlet />
+      
     </AppProvider>
   );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import BasicPie from "../components/pieChart";
+import BasicScatter from "../components/scatter";
+
+
+
+
 
 export default function Dashboard() {
   return (
+    <>
     <div>
       <h1>Dashboard</h1>
     </div>
+    <div id="charts">
+      <BasicPie></BasicPie>
+      <BasicScatter></BasicScatter>
+    </div>
+    </>
   );
 }
