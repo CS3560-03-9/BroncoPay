b261a2fa48b0ff31dc7964b56f0f821f23dd7012
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2024-11-20 21:32:06 -0800

    Updated Subscription Controller

--- server/controllers/subscriptions.js
+++ server/controllers/subscriptions.js
@@ -1,24 +1,35 @@
 const db = require('../utils/db');
-const accountsController = require('./accounts')
+const accountsController = require('./accounts');
 
 async function getSubscriptionsByAccount(handler) {
-	return await db.query('SELECT * FROM `subscriptions` WHERE `from_handler` = ?', [handler]);
+    return await db.query('SELECT * FROM `subscriptions` WHERE `handler` = ?', [handler]);
 }
 
 async function createSubscription(body) {
-	const fromAccount = await accountsController.getAccount(body.fromHandler);
-	const subscriptionInfo = undefined; // TODO GET SUB INFO
-	if (fromAccount.length === 0) {
-		throw new Error('account does not exist');
-	}
-	if (subscriptionInfo.amount > fromAccount[0].spending_limit) {
-		throw new Error('amount is greater than spending limit');
-	}
-	return await db.query('INSERT INTO `subscriptions` (`from_handler`, `to_handler`, `amount`, `transaction_desc`) VALUES (?, ?, ?, ?)',
-		[body.fromHandler, body.toHandler, body.amount, body.description]);
+    const fromAccount = await accountsController.getAccount(body.fromHandler);
+    if (fromAccount.length === 0) {
+        throw new Error('account does not exist');
+    }
+
+    const pledgeInfo = await getPledgeInfo(body.pledgeId);
+    if (!pledgeInfo) {
+        throw new Error('pledge information not found');
+    }
+
+    if (pledgeInfo.cost > fromAccount[0].spending_limit) {
+        throw new Error('amount is greater than spending limit');
+    }
+
+    return await db.query('INSERT INTO `subscriptions` (`handler`, `pledge_id`, `amount`, `transaction_desc`) VALUES (?, ?, ?, ?)',
+        [body.handler, body.pledgeId, pledgeInfo.cost, body.description]);
+}
+
+async function getPledgeInfo(pledgeId) {
+    return await db.query('SELECT * FROM `pledges` WHERE `pledge_id` = ?', [pledgeId]);
 }
 
 module.exports = {
-	getSubscriptionsByAccount,
-	createSubscription,
+    getSubscriptionsByAccount,
+    createSubscription,
+    getPledgeInfo,
 };
