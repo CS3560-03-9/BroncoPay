037991b8653762021a853d2a550e9b144d6498f1
apixys <argentumapixys@gmail.com>
2024-12-02 11:06:46 -0800

    reformatted code

--- server/controllers/subscriptions.js
+++ server/controllers/subscriptions.js
@@ -2,34 +2,34 @@ const db = require('../utils/db');
 const accountsController = require('./accounts');
 
 async function getSubscriptionsByAccount(handler) {
-    return await db.query('SELECT * FROM `subscriptions` WHERE `handler` = ?', [handler]);
+	return await db.query('SELECT * FROM `subscriptions` WHERE `handler` = ?', [handler]);
 }
 
 async function createSubscription(body) {
-    const fromAccount = await accountsController.getAccount(body.fromHandler);
-    if (fromAccount.length === 0) {
-        throw new Error('account does not exist');
-    }
+	const fromAccount = await accountsController.getAccount(body.fromHandler);
+	if (fromAccount.length === 0) {
+		throw new Error('account does not exist');
+	}
 
-    const pledgeInfo = await getPledgeInfo(body.pledgeId);
-    if (!pledgeInfo) {
-        throw new Error('pledge information not found');
-    }
+	const pledgeInfo = await getPledgeInfo(body.pledgeId);
+	if (!pledgeInfo) {
+		throw new Error('pledge information not found');
+	}
 
-    if (pledgeInfo.cost > fromAccount[0].spending_limit) {
-        throw new Error('amount is greater than spending limit');
-    }
+	if (pledgeInfo.cost > fromAccount[0].spending_limit) {
+		throw new Error('amount is greater than spending limit');
+	}
 
-    return await db.query('INSERT INTO `subscriptions` (`handler`, `pledge_id`, `amount`, `transaction_desc`) VALUES (?, ?, ?, ?)',
-        [body.handler, body.pledgeId, pledgeInfo.cost, body.description]);
+	return await db.query('INSERT INTO `subscriptions` (`handler`, `pledge_id`, `amount`, `transaction_desc`) VALUES (?, ?, ?, ?)',
+		[body.handler, body.pledgeId, pledgeInfo.cost, body.description]);
 }
 
 async function getPledgeInfo(pledgeId) {
-    return await db.query('SELECT * FROM `pledges` WHERE `pledge_id` = ?', [pledgeId]);
+	return await db.query('SELECT * FROM `pledges` WHERE `pledge_id` = ?', [pledgeId]);
 }
 
 module.exports = {
-    getSubscriptionsByAccount,
-    createSubscription,
-    getPledgeInfo,
+	getSubscriptionsByAccount,
+	createSubscription,
+	getPledgeInfo,
 };
