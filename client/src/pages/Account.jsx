a0cfa98800829c1f68361d6f158d885c760c403b
PattyCakesxD <nguyenpatty42@gmail.com>
2024-12-02 18:46:57 -0800

    organizes the sidebar

---
+++ client/src/pages/Account.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+import { Typography, Box } from "@mui/material";
+
+export default function Account() {
+  return (
+    <Box>
+      <Typography variant="h1">Account</Typography>
+    </Box>
+  );
+}
