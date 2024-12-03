a6372178a6d9843c49a99b15af97a26982fb9304
PattyCakesxD <nguyenpatty42@gmail.com>
2024-12-02 20:20:54 -0800

    eslint

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -45,6 +45,7 @@ export default function Dashboard() {
       }
     };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handlePayment = () => {
--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -44,6 +44,7 @@ export default function Payment() {
       }
     };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (loading || error) {
