5ba3986948716f5c1eb1dacc74f44416e0f8ac5a
PattyCakesxD <nguyenpatty42@gmail.com>
2024-12-04 19:25:12 -0800

    fixes endpoint status code

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -1,145 +1,158 @@
-const db = require('../utils/db');
+const db = require("../utils/db");
 
 getAllAccounts = async (req, res) => {
-    try {
-        const accounts = await db.query('SELECT * FROM `accounts`');
-        res.status(200).json({
-            status:'success',
-            data: {
-                accounts: accounts,
-            },
-        })
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err.message,
-        })
-    }
-}
+  try {
+    const accounts = await db.query("SELECT * FROM `accounts`");
+    res.status(200).json({
+      status: "success",
+      data: {
+        accounts: accounts,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
 getHandlerAccount = async (req, res) => {
-    const {handler} = req.params;
-    if (!handler) {
-        res.status(400).json({
-            status: 'fail',
-            data: {
-                handler: 'Handler is missing',
-            },
-        })
+  const { handler } = req.params;
+  if (!handler) {
+    res.status(400).json({
+      status: "fail",
+      data: {
+        handler: "Handler is missing",
+      },
+    });
+  }
+  try {
+    const account = await db.query(
+      "SELECT handler, email, balance, spending_limit FROM `accounts`"
+    );
+    if (account.length === 0) {
+      res.status(404).json({
+        status: "fail",
+        data: {
+          handler: "Account not found",
+        },
+      });
     }
-    try {
-        const account = await db.query('SELECT handler, email, balance, spending_limit FROM `accounts`');
-        if (account.length === 0) {
-            res.status(404).json({
-                status: 'fail',
-                data: {
-                    handler: 'Account not found',
-                }
-            });
-        }
-        res.status(200).json({
-            status: 'success',
-            data: {
-                account: account,
-            }
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err.message,
-        });
-    }
-}
+    res.status(200).json({
+      status: "success",
+      data: {
+        account: account,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
 
 updateSpendingLimit = async (req, res) => {
-    const {handler} = req.params;
-    limit = req.body.spendingLimit;
+  const { handler } = req.params;
+  limit = req.body.spendingLimit;
 
-    limit = parseFloat(limit);
+  limit = parseFloat(limit);
 
-    if (!handler) {
-        res.status(400).json({
-            status:'fail',
-            data: {
-                handler: 'Handler is missing',
-            }
-        });
+  if (!handler) {
+    res.status(400).json({
+      status: "fail",
+      data: {
+        handler: "Handler is missing",
+      },
+    });
+  }
+
+  try {
+    const account = await db.query(
+      "SELECT * FROM `accounts` WHERE `handler` = ?",
+      [handler]
+    );
+    if (account.length === 0) {
+      res.status(404).json({
+        status: "fail",
+        data: {
+          handler: "Account not found",
+        },
+      });
     }
 
-    try {
-        const account = await db.query('SELECT * FROM `accounts` WHERE `handler` = ?', [handler]);
-        if (account.length === 0) {
-            res.status(404).json({
-                status: 'fail',
-                data: {
-                    handler: 'Account not found',
-                },
-            });
-        }
-        
-        console.log(limit);
-        if (!isNaN(limit) && isFinite(limit)) {
-            const limitTest = await db.query('UPDATE `accounts` SET `spending_limit` = ? WHERE `handler` = ?', [limit, handler]);
-            
-            res.status(204).json({
-                status: 'success',
-                data: {
-                    handler: 'Spending limit updated',
-                },
-            });
-        } else {
-            res.status(400).json({
-                status:'fail',
-                data: {
-                    handler: 'Invalid Limit'
-                }
-            })
-        }
-        
-    } catch(err) {
-        res.status(500).json({
-            status:'error',
-            message: err.message,
-        });
+    console.log(limit);
+    if (!isNaN(limit) && isFinite(limit)) {
+      const limitTest = await db.query(
+        "UPDATE `accounts` SET `spending_limit` = ? WHERE `handler` = ?",
+        [limit, handler]
+      );
+
+      res.status(200).json({
+        status: "success",
+        data: {
+          handler: "Spending limit updated",
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: "fail",
+        data: {
+          handler: "Invalid Limit",
+        },
+      });
     }
-}
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
 
 deleteAccount = async (req, res) => {
-    const {handler} = req.params;
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+  const { handler } = req.params;
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!handler) {
-        res.status(400).json({
-            status: 'fail',
-            data: {
-                handler: 'handler is missing',
-            },
-        });
-    }
+  if (!handler) {
+    res.status(400).json({
+      status: "fail",
+      data: {
+        handler: "handler is missing",
+      },
+    });
+  }
 
-    try {
-        const deletion = await db.query('DELETE FROM `accounts` WHERE `handler` = ?', [handler]); 
+  try {
+    const deletion = await db.query(
+      "DELETE FROM `accounts` WHERE `handler` = ?",
+      [handler]
+    );
 
-        res.status(204).json({
-            status: 'success',
-            data: 'Account Deletion Successful'
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 'error',
-            message: err.message,
-        });
-    }
-}
+    res.status(204).json({
+      status: "success",
+      data: "Account Deletion Successful",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
 
 async function changeBalance(handler, amount) {
-    return await db.query('UPDATE `accounts` SET `balance` = `balance` + ? WHERE `handler` = ?', [amount, handler]);
+  return await db.query(
+    "UPDATE `accounts` SET `balance` = `balance` + ? WHERE `handler` = ?",
+    [amount, handler]
+  );
 }
 
 module.exports = {
-    getAllAccounts,
-    getHandlerAccount,
-    updateSpendingLimit,
-    deleteAccount,
-    changeBalance,
+  getAllAccounts,
+  getHandlerAccount,
+  updateSpendingLimit,
+  deleteAccount,
+  changeBalance,
 };
