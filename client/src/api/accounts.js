f7086d370cbbb55b99507246ab97f618f4149773
PattyCakesxD <nguyenpatty42@gmail.com>
2024-12-04 19:21:59 -0800

    adds function to update account spending limit

--- client/src/api/accounts.js
+++ client/src/api/accounts.js
@@ -1,9 +1,12 @@
-const BASE_URL = "http://localhost:3000"; // ** TODO: Update this in env or do some default url shenanigans later **
+const BASE_URL = "http://localhost:3000";
 
-export async function fetchUser(user) {
+/**
+ * Retrieves a user by their handler
+ * @param {*} handler
+ */
+export async function fetchUser(handler) {
   try {
-    const response = await fetch(`${BASE_URL}/accounts/${user}`, {
-      // ** TODO: Update URL to use a base url rather than hardcode (env?)
+    const response = await fetch(`${BASE_URL}/accounts/${handler}`, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -15,6 +18,33 @@ export async function fetchUser(user) {
     }
 
     return data.account;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+/**
+ * Edits a user's information
+ * @param {*} handler
+ * @param {*} newLimit
+ */
+export async function updateSpendingLimit(handler, newLimit) {
+  try {
+    const response = await fetch(`${BASE_URL}/accounts/${handler}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ spendingLimit: parseInt(newLimit) }),
+    });
+    const { status, data } = await response.json();
+    console.log("res", response);
+
+    if (!response.ok || status !== "success") {
+      throw new Error("Failed to update spending limit");
+    }
+
+    return data;
   } catch (err) {
     throw new Error(err);
   }
